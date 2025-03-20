25d3cb24f88e59a7baecb31758df9baac6638d6a
srizonsansar <srizonsansar@gmail.com>
2025-03-20 11:09:53 +0000

    I am not a Robot Project done

--- 1-i-am-not-a-robot/script.js
+++ 1-i-am-not-a-robot/script.js
@@ -1,5 +1,42 @@
 const checkbox = document.getElementById('checkbox')
+const submitBtn = document.querySelector('button[type=submit]')
 
-checkbox.addEventListener('click', function(){
-    console.log('Welcome')
+checkbox.disabled = true
+submitBtn.disabled = true
+
+const elements = document.querySelectorAll('.element')
+const selectColor = document.getElementById('SelectColor')
+
+// Assign color to elements
+elements.forEach( (element) => {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.innerHTML = color
+    selectColor.innerHTML = color
+})
+
+// Generate Random Color
+function getRandomColor(){
+    const letter = '0123456789ABCDEF'
+    let color = '#'
+    for ( let i = 0; i < 6; i++ ){
+        color += letter[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+// check if right color
+elements.forEach( (element) => {
+    element.addEventListener('click', function(){
+        if (element.innerHTML === selectColor.innerHTML) {
+            checkbox.checked = true
+            submitBtn.disabled = false
+            submitBtn.classList.remove('btn-light')
+            submitBtn.classList.add('btn-success')
+        } else {
+            location.reload(true)
+            checkbox.disabled = true
+            submitBtn.disabled = true
+        }
+    })
 })
