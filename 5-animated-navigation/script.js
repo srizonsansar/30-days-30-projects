057d9b3ec8edb2acd344ce2a2c7601eef4585252
Navaraj Subedi <srizonsansar@gmail.com>
2025-04-03 23:48:16 +0100

    Working on Nav JS

---
+++ 5-animated-navigation/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('#menu-toggle')
+const activeElements = document.querySelectorAll('.active-elements')
+
+menuToggle.addEventListener('click', function(){
+    
+})
